c43a2f83292f2887d31a242e92c075329592972d
gui <guilherme.deck1227@gmail.com>
2023-02-24 16:47:55 -0300

    pushing tweet on screen

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,26 +1,35 @@
+import { FormEvent, useState } from 'react'
 import { Tweet } from '../components/Tweet'
 import { Header } from '../components/Header'
 import { Divider } from '../components/Divider'
-import { RouterProvider } from 'react-router-dom'
-import {router} from "../routes"
 import "./Timeline.css"
 
-const tweets = [
-  "first data",
-  "teste",
-  "New tweet"
-]
-
+let newTweet = ""
 export function Timeline(){
+  const [tweets, setTweets] = useState(
+ ["first data",
+  "teste",
+  "New tweet"])
+
+function createNewTweet(event: FormEvent){
+event.preventDefault()
+setTweets([newTweet,...tweets])
+}
+
 return(
   <main className="timeline">
   <Header title='Home'/>
 
   {/* <RouterProvider router={router}/> */}
-  <form className="new-tweet-form">
+  <form onSubmit={createNewTweet} className="new-tweet-form">
     <label htmlFor="tweet">
       <img src="https://github.com/GuiDecker.png" alt="Guilherme Dev" />
-      <textarea id='tweet' placeholder="What's happening"/>
+      <textarea 
+      id='tweet'
+       placeholder="What's happening" 
+      onChange={(event)=>{
+        newTweet = event.target.value
+      }}/>
     </label>
     <button type='submit'>Tweet</button>
   </form>
